ccdd8d7d1c952bd0d4061f829d56e951611d7f41
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-08-27 21:36:21 -0500

    Add files via upload

--- Project 21/sketch.js
+++ Project 21/sketch.js
@@ -24,7 +24,12 @@ function setup() {
 		density:1.2
 	}
 
-	Bodies.circle();
+	//groundObj.display();
+	groundObj = new ground(width/2, 670, width, 20);
+	leftSide = new ground(1100, 600, 20, 120);
+	rightSide = new ground(2200, 600, 20, 120);
+
+	//Bodies.circle();
 
 	//Create the Bodies Here.
 	ball = Bodies.circle(200,50,10,ball_options);
